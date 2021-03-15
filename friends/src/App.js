75b5ad71a40c3dd206facda768371ea10bd2c994
XavierTwitty <x.twitty@gmail.com>
2021-03-15 15:41:38 -0400

    first commit

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Route>
+      <div className="App"></div>
+    </Route>
+  );
+}
+
+export default App;
